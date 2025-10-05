f871ad5854dc7a9cd2b479fc8dfaab92ca14291e
hev <git@hev.cc>
2025-10-06 01:03:13 +0800

    Build: JNI: Update Android platform to 29.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -14,7 +14,7 @@
 #
 
 APP_OPTIM := release
-APP_PLATFORM := android-21
+APP_PLATFORM := android-29
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_CFLAGS := -O3 -DPKGNAME=hev/socks5
 APP_CPPFLAGS := -O3 -std=c++11
